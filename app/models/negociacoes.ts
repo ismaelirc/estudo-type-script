373a1b31dd68831e829bbe4f57d58d372de0fb7a
Ismael Costa <ismael@Ismaels-MacBook-Pro.local>
2023-04-01 09:41:55 -0300

    Firs commit on typescript

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,17 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes{
+    
+    private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao){
+
+        this.negociacoes.push(negociacao);
+    
+    }
+
+    listar(): ReadonlyArray<Negociacao>{
+        return this.negociacoes;
+    }
+
+}
